492bb4f55eb3515a2c1ed82888b27f231d4ec80e
petrusgf <petrusgf@gmail.com>
2016-05-24 01:44:22 -0700

    Update and rename PowerShell 2. all_installations.ps1 to 2.PowerShell all_installations.ps1

--- 2.PowerShell all_installations.ps1
+++ 2.PowerShell all_installations.ps1
@@ -1,2 +1,2 @@
-#Invoke file "PowerShell all_installations.ps1"
+#Invoke file "PowerShell all_installations.ps1" in windows terminal
 Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, Publisher, InstallDate
